29d1d337b5c8ba71704caf96c18960ed82b1cdc4
farah-elsayed <farahfarahat2001@gmail.com>
2022-01-02 21:50:48 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Toyz.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+        public ActionResult Icon()
+        {
+            return View();
+        }
+        public ActionResult payment()
+        {
+            return View();
+        }
+        public ActionResult product()
+        {
+            return View();
+        }
+        public ActionResult service()
+        {
+            return View();
+        }
+        public ActionResult shop()
+        {
+            return View();
+        }
+        public ActionResult single()
+        {
+            return View();
+        }
+        public ActionResult typography()
+        {
+            return View();
+        }
+
+    }
+}
